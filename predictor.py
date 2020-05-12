26cf0d8a8006070e700e324aff6d44a06170a24a
Zwitterion01 <37821373+Zwitterion01@users.noreply.github.com>
2020-05-12 10:13:14 -0400

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -3,4 +3,4 @@ def predictor(query):
  loaded_model = joblib.load('SVMClassifier.sav')
  result = loaded_model.predict_proba([query])
  prob_per_class_dictionary_svm = sorted(zip(loaded_model.classes_, result[0]), key=lambda x: x[1], reverse=True)
- print(prob_per_class_dictionary_svm)
+ return prob_per_class_dictionary_svm
