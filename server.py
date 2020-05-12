ad79e85cadb447aa527b9cc7c7672b70ad945354
Zwitterion01 <37821373+Zwitterion01@users.noreply.github.com>
2020-05-12 11:33:02 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 from recommender import Recommender
 import predictor
 import json
@@ -9,7 +10,7 @@ import numpy as np
 app = Flask(__name__)
 categories = ['ENTERTAINMENT', 'FOOD & DRINK', 'POLITICS', 'STYLE & BEAUTY', 'TECH']
 tweets = []
-with open('News_Category_Dataset_v2.json') as f:
+with open('data.json') as f:
     for jsonObject in f:
         tweet = json.loads(jsonObject)
         tweets.append(tweet)
@@ -27,9 +28,11 @@ def index():
     print(666666666)
     return 'GOOODDD!!!', 200
 
-@app.route('/process/<headline>/<description>')
-def process(headline, description):
+@app.route('/process/')
+def process():
     tweets_filtered = []
+    headline=request.args.get('headline')
+    description=request.args.get('short_description')
     query = headline + ' ' + description
     prediction = predictor.predictor(query)
     prediction = sorted(prediction, key= lambda i:i[1], reverse=True)
@@ -47,4 +50,5 @@ def workload(count):
     return 'success', 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
